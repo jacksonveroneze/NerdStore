49cf85a1c17c15e952e5f5a36324b241f58db98d
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-09-01 21:13:08 -0300

    Ajustes.

--- src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -27,6 +27,15 @@ namespace NerdStore.Catalogo.Domain.Events
         public async Task Handle(PedidoIniciadoEvent notification, CancellationToken cancellationToken)
         {
             var result = await _estoqueService.DebitarListaProdutosPedido(notification.ProdutosPedido);
+
+            if (result)
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueConfirmadoEvent(message.PedidoId, message.ClienteId, message.Total, message.ProdutosPedido, message.NomeCartao, message.NumeroCartao, message.ExpiracaoCartao, message.CvvCartao));
+            }
+            else
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueRejeitadoEvent(message.PedidoId, message.ClienteId));
+            }
         }
     }
 }
